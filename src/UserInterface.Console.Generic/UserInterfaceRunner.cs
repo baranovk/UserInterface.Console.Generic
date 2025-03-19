31b6b9bf36e7af59f474ae388e1545a514e8a07e
baranovk <baranovk@yandex.ru>
2025-03-19 16:47:08 +0300

    Изменение типа параметра UserInterfaceRunner.RunAsync с Func на InteractionScenario

--- src/UserInterface.Console.Generic/UserInterfaceRunner.cs
+++ src/UserInterface.Console.Generic/UserInterfaceRunner.cs
@@ -5,10 +5,10 @@ namespace UserInterface.Console.Generic;
 
 public static class UserInterfaceRunner
 {
-    public static async Task RunAsync(Func<InteractionScenario> buildEntryPointScenario)
+    public static async Task RunAsync(InteractionScenario entryPointScenario)
         => await new ConsoleInterface()
                     .Pipe(@interface
-                            => Async(new Context(new UserInterface(@interface), buildEntryPointScenario()))
+                            => Async(new Context(new UserInterface(@interface), entryPointScenario))
                                 .IterateUntilAsync(
                                     async ctx => await ctx.CurrentScenario.Execute(ctx).ConfigureAwait(false),
                                     ctx => ctx.Finished
