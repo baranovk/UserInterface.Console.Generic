9117e4fac1d44c6d58fe7d42855ba0c49fa25d9a
baranovk <baranovk@yandex.ru>
2025-03-20 15:37:11 +0300

    Доработка UserInterfaceRunne - обработка ошибок без завершения приложения

--- src/UserInterface.Console.Generic/UserInterfaceRunner.cs
+++ src/UserInterface.Console.Generic/UserInterfaceRunner.cs
@@ -10,7 +10,13 @@ public static class UserInterfaceRunner
                     .Pipe(@interface
                             => Async(new Context(new UserInterface(@interface), entryPointScenario))
                                 .IterateUntilAsync(
-                                    async ctx => await ctx.CurrentScenario.Execute(ctx).ConfigureAwait(false),
+                                    async ctx => (await TryAsync(() => ctx.CurrentScenario.Execute(ctx)).RunAsync().ConfigureAwait(false))
+                                        .Match(
+                                            ex => ctx.UI.WriteEmpty()
+                                                        .WriteMessage(ex.ToString())
+                                                        .WriteEmpty()
+                                                        .Pipe(_ => ctx with { CurrentScenario = entryPointScenario }),
+                                            @ctx => @ctx),
                                     ctx => ctx.Finished
                                 )
                     ).ConfigureAwait(false);
