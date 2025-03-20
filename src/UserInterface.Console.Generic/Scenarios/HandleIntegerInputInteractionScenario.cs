7c511ce4ef0aba1d80888478f644d19997ccf138
baranovk <baranovk@yandex.ru>
2025-03-20 15:33:33 +0300

    Добавлен generic-сценарий HandleIntegerInputInteractionScenario

---
+++ src/UserInterface.Console.Generic/Scenarios/HandleIntegerInputInteractionScenario.cs
@@ -0,0 +1,37 @@
+using static Functional.F;
+
+namespace UserInterface.Console.Generic.Scenarios;
+
+public class HandleIntegerInputInteractionScenario(
+    string prompt,
+    string invalidInputMessage,
+    InteractionScenario onCancelScenario,
+    Func<IntegerInput, Task<Context>> inputHandler,
+    string? cancelKey = null) : InteractionScenario
+{
+    private readonly string _prompt = prompt;
+    private readonly string _cancelKey = cancelKey ?? Resources.QuitKey;
+    private readonly string _invalidInputMessage = invalidInputMessage;
+    private readonly InteractionScenario _onCancelScenario = onCancelScenario;
+    private readonly Func<IntegerInput, Task<Context>> _inputHandler = inputHandler;
+
+    public override async Task<Context> Execute(Context context)
+        => await (await AwaitInput(
+                context.UI,
+                input => input switch
+                {
+                    IntegerInput i => Async(Valid(i as UserInput)),
+                    TextInput ti => Async(_cancelKey == ti.Value ? Valid(ti as UserInput) : Invalid<UserInput>(Error(_invalidInputMessage))),
+                    _ => Async(Invalid<UserInput>(Error(_invalidInputMessage)))
+                },
+                _prompt
+            )
+            .ConfigureAwait(false)
+        )
+        .Pipe(input => input switch
+            {
+                IntegerInput i => _inputHandler(i),
+                _ => Async(context with { CurrentScenario = _onCancelScenario })
+            }
+        ).ConfigureAwait(false);
+}
