84d3b50730ba01e5ac7317cbdc1414d5fd8f23aa
baranovk <baranovk@yandex.ru>
2025-04-30 11:55:12 +0300

    Добавлен метод расширения IsQuitKey

--- src/UserInterface.Console.Generic/Extensions.cs
+++ src/UserInterface.Console.Generic/Extensions.cs
@@ -9,4 +9,6 @@ public static class Extensions
 
     public static string Format(this string template, IFormatProvider formatProvider, params object?[] args)
         => string.Format(CultureInfo.InvariantCulture, template, args);
+
+    public static bool IsQuitKey(this TextInput input) => input.Value == Resources.QuitKey;
 }
