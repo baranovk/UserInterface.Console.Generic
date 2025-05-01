1cf067a5592215a8c6d734292757257a8e5e8b83
baranovk <baranovk@yandex.ru>
2025-05-01 11:00:09 +0300

    Добавлен класс StatefullInteractionScenario

--- src/UserInterface.Console.Generic/Scenarios/StatefullInteractionScenario.cs
+++ src/UserInterface.Console.Generic/Scenarios/StatefullInteractionScenario.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace UserInterface.Console.Generic.Scenarios;
 
 public abstract class StatefullInteractionScenario<TState>(TState initialState) : InteractionScenario
