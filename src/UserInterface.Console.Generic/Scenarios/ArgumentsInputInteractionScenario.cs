7a530e49cb5d509e65a45aa00af79a136a5caf4b
baranovk <baranovk@yandex.ru>
2025-06-08 10:52:40 +0300

    Небольшой фикс - наследование ArgumentsInputInteractionScenario от InteractionScenario

--- src/UserInterface.Console.Generic/Scenarios/ArgumentsInputInteractionScenario.cs
+++ src/UserInterface.Console.Generic/Scenarios/ArgumentsInputInteractionScenario.cs
@@ -1,8 +1,8 @@
 namespace UserInterface.Console.Generic.Scenarios;
 
-public abstract class ArgumentsInputInteractionScenario(IDictionary<string, string> arguments)
+public abstract class ArgumentsInputInteractionScenario(IDictionary<string, string> arguments) : InteractionScenario
 {
     protected IDictionary<string, string> Arguments { get; private set; } = arguments;
 
-    public abstract Task<Context> Execute(Context context);
+    public abstract override Task<Context> Execute(Context context);
 }
